c91dab3ceb6aa6b14be869b6e8a610cf070bf52a
Nelson W Foltz <nelsonwfoltz@Nelsons-iMac.fios-router.home>
2019-08-03 09:23:55 -0400

    deleted test component after refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FundamentalNgxModule } from 'fundamental-ngx';
-import { TableCdkExampleComponent } from './table-cdk-example/table-cdk-example.component';
 import { MatTableModule } from '@angular/material';
 import { CdkTableModule } from '@angular/cdk/table';
 import { PricingLookupComponent } from './pricing-lookup/pricing-lookup.component';
 
 @NgModule({
-  declarations: [AppComponent, TableCdkExampleComponent, PricingLookupComponent],
+  declarations: [AppComponent, PricingLookupComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
