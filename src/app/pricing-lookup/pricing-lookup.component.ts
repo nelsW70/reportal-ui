c01d156f4c525a6c2c25465c9dbb17810797b5b2
Nelson W Foltz <nelsonwfoltz@Nelsons-iMac.fios-router.home>
2019-08-03 15:36:25 -0400

    cleanup on component ts

--- src/app/pricing-lookup/pricing-lookup.component.ts
+++ src/app/pricing-lookup/pricing-lookup.component.ts
@@ -112,14 +112,7 @@ const data: Data[] = [
   }
 ];
 
-/**
- * Data source to provide what data should be rendered in the table. The observable provided
- * in connect should emit exactly the data that should be rendered by the table. If the data is
- * altered, the observable should emit that new set of data on the stream. In our case here,
- * we return a stream that contains only one set of data that doesn't change.
- */
 export class MobileDataSource extends DataSource<any> {
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
   connect(): Observable<Data[]> {
     const rows = [];
     data.forEach(data => rows.push(data, { detailRow: true, data }));
