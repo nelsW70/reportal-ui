2e05f78e9e96c64252f7d4b34e85612f91ebe56e
Nelson Foltz <NFoltz@ppg.com>
2019-09-04 09:23:35 -0400

    starting with pricing lookup content as template in invoice lookup component

--- src/app/invoice-lookup/invoice-lookup.component.ts
+++ src/app/invoice-lookup/invoice-lookup.component.ts
@@ -1,15 +1,179 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable, of } from 'rxjs';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
+export interface ProductInfo {
+  itemId: string;
+  itemDescription: string;
+  currentPrice: string;
+  priceSource: string;
+  final: string;
+  mgplPrice: string;
+  matrixPrice: string;
+}
+
+const data: ProductInfo[] = [
+  {
+    itemId: '6-411/01',
+    itemDescription: 'S-Hide INT LX EGG WHPB 6-411 B100',
+    currentPrice: '$18.85',
+    priceSource: 'GPQ',
+    final: 'N',
+    mgplPrice: '$18.85',
+    matrixPrice: '$25.75'
+  },
+  {
+    itemId: 'AK2LV-B/55',
+    itemDescription: 'AMERLOCK 2 LV CUBE',
+    currentPrice: '$18.85',
+    priceSource: 'GPQ',
+    final: '-',
+    mgplPrice: '$18.85',
+    matrixPrice: '$25.75'
+  },
+  {
+    itemId: 'AK2-T1/01',
+    itemDescription: 'AMERLOCK 2/400 DEEP TINT RESIN',
+    currentPrice: '$18.85',
+    priceSource: '-',
+    final: 'Y',
+    mgplPrice: '-',
+    matrixPrice: '$25.75'
+  },
+  {
+    itemId: '6-411/01',
+    itemDescription: 'S-HIDE INT LX EGG WHPB 6-411 B100',
+    currentPrice: '$18.85',
+    priceSource: 'GPQ',
+    final: 'N',
+    mgplPrice: '$18.85',
+    matrixPrice: '$25.75'
+  },
+  {
+    itemId: '6-411/01',
+    itemDescription: 'S-HIDE INT LX EGG WHPB 6-411 B100',
+    currentPrice: '$18.85',
+    priceSource: 'MGPL',
+    final: 'N',
+    mgplPrice: '$18.85',
+    matrixPrice: '$25.75'
+  },
+  {
+    itemId: '6-411/01',
+    itemDescription: 'S-HIDE INT LX EGG WHPB 6-411 B100',
+    currentPrice: 'NOT FOUND',
+    priceSource: 'GPQ',
+    final: 'N',
+    mgplPrice: 'NOT FOUND',
+    matrixPrice: '$25.75'
+  },
+  {
+    itemId: '6-411/01',
+    itemDescription: 'S-HIDE INT LX EGG WHPB 6-411 B100',
+    currentPrice: '$18.85',
+    priceSource: '-',
+    final: '-',
+    mgplPrice: '$18.85',
+    matrixPrice: '$25.75'
+  },
+  {
+    itemId: '6-411/01',
+    itemDescription: 'S-HIDE INT LX EGG WHPB 6-411 B100',
+    currentPrice: '$18.85',
+    priceSource: 'GPQ',
+    final: 'Y',
+    mgplPrice: '$18.85',
+    matrixPrice: '$25.75'
+  },
+  {
+    itemId: 'AK2LV-B/55',
+    itemDescription: 'S-HIDE INT LX EGG WHPB 6-411 B100',
+    currentPrice: '$18.85',
+    priceSource: 'GPQ',
+    final: 'N',
+    mgplPrice: '$18.85',
+    matrixPrice: 'NOT FOUND'
+  },
+  {
+    itemId: 'AK2LV-B/55',
+    itemDescription: 'S-HIDE INT LX EGG WHPB 6-411 B100',
+    currentPrice: '$18.85',
+    priceSource: 'GPQ',
+    final: 'N',
+    mgplPrice: 'NOT FOUND',
+    matrixPrice: '$25.75'
+  }
+];
+
+export class MobileDataSource extends DataSource<any> {
+  connect(): Observable<ProductInfo[]> {
+    const rows = [];
+    data.forEach(product => rows.push(product, { detailRow: true, product }));
+    return of(rows);
+  }
+
+  disconnect() {}
+}
 
 @Component({
   selector: 'app-invoice-lookup',
   templateUrl: './invoice-lookup.component.html',
-  styleUrls: ['./invoice-lookup.component.scss']
+  styleUrls: ['./invoice-lookup.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state(
+        'collapsed',
+        style({ height: '0px', minHeight: '0', visibility: 'hidden' })
+      ),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      )
+    ])
+  ]
 })
 export class InvoiceLookupComponent implements OnInit {
+  currentWindowWidth: number;
+  currentIndex: number = null;
+  // fake combobox values and auto-complete for demo
+  searchTermOne = '';
+  searchTermTwo = '';
+  dropdownValues1 = ['a', 'b', 'c', 'd'];
+  dropdownValues2 = ['a', 'b', 'c', 'd'];
 
-  constructor() { }
+  // fake values for semantic ui select
+  selectedOption = [];
+  options: Array<string> = ['one', 'two', 'three', 'four', 'five'];
+
+  selectedOption2 = [];
+  options2: Array<string> = ['one', 'two', 'three', 'four', 'five'];
+
+  expandedData: any;
+  displayedColumns = ['itemId', 'currentPrice', 'priceSource'];
+  dataSource = new MobileDataSource();
+
+  isExpansionDetailRow = (i: number, row: object) =>
+    row.hasOwnProperty('detailRow');
+
+  constructor() {}
 
   ngOnInit() {
+    this.currentWindowWidth = window.innerWidth;
   }
 
+  // fake for demo to calculate window size on resize and show/different size screen
+  // this would be expensive if anyone resizes and should only be implemented in the onInit
+  // method if used
+  @HostListener('window:resize')
+  onResize() {
+    this.currentWindowWidth = window.innerWidth;
+  }
 }
