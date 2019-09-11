b61e019e9374394f60d3d0159e1a7315a5688062
Nelson W Foltz <nelsonwfoltz@Nelsons-iMac.fios-router.home>
2019-09-11 10:24:21 -0400

    building out top panel of inventory lookup

--- src/app/inventory-lookup/inventory-lookup.component.ts
+++ src/app/inventory-lookup/inventory-lookup.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory-lookup.component.scss']
 })
 export class InventoryLookupComponent implements OnInit {
+  // fake combobox values and auto-complete for demo
+  searchTermOne = '';
+  dropdownValues1 = ['a', 'b', 'c', 'd'];
 
-  constructor() { }
+  // fake values for semantic ui select
+  selectedOption = [];
+  options: Array<string> = ['one', 'two', 'three', 'four', 'five'];
 
-  ngOnInit() {
-  }
+  selectedOption2 = [];
+  options2: Array<string> = ['one', 'two', 'three', 'four', 'five'];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
