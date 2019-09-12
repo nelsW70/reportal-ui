17c14576271bd36c60c6560d1c70aab7cf99a113
Nelson Foltz <NFoltz@ppg.com>
2019-09-12 15:36:16 -0400

    cleanup on distribution model

--- src/app/inventory-lookup-distribution/inventory-lookup-distribution.component.ts
+++ src/app/inventory-lookup-distribution/inventory-lookup-distribution.component.ts
@@ -1,125 +1,55 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import { DataSource } from '@angular/cdk/collections';
-import { Observable, of } from 'rxjs';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from '@angular/animations';
-
 export interface ProductInfo {
   itemNumber: string;
-  storeNumber: string;
-  address1: string;
-  address2: string;
-  address3: string;
-  address4: string;
+  storeName: string;
   itemCount: string;
-  phoneNumber: string;
-  distance: string;
 }
 
 const data: ProductInfo[] = [
   {
     itemNumber: '',
-    storeNumber: 'PPG Paints 8089',
-    address1: '921 East Ventura Blvd.',
-    address2: 'Oxnard',
-    address3: 'CA',
-    address4: '93036',
-    itemCount: '2536',
-    phoneNumber: '(805) 485-9556',
-    distance: '0.02 miles'
+    storeName: 'Riverside',
+    itemCount: '2536'
   },
   {
     itemNumber: '',
-    storeNumber: 'PPG Paints 8089',
-    address1: '921 East Ventura Blvd.',
-    address2: 'Oxnard',
-    address3: 'CA',
-    address4: '93036',
-    itemCount: '0',
-    phoneNumber: '(805) 485-9556',
-    distance: '0.02 miles'
+    storeName: 'Reading',
+    itemCount: '0'
   },
   {
     itemNumber: '',
-    storeNumber: 'PPG Paints 8089',
-    address1: '921 East Ventura Blvd.',
-    address2: 'Oxnard',
-    address3: 'CA',
-    address4: '93036',
-    itemCount: '3652',
-    phoneNumber: '(805) 485-9556',
-    distance: '0.02 miles'
+    storeName: 'Oakwood',
+    itemCount: '3652'
   },
   {
     itemNumber: '',
-    storeNumber: 'PPG Paints 8089',
-    address1: '921 East Ventura Blvd.',
-    address2: 'Oxnard',
-    address3: 'CA',
-    address4: '93036',
-    itemCount: '0',
-    phoneNumber: '(805) 485-9556',
-    distance: '0.02 miles'
+    storeName: 'McCarran',
+    itemCount: '0'
   },
   {
     itemNumber: '',
-    storeNumber: 'PPG Paints 8089',
-    address1: '921 East Ventura Blvd.',
-    address2: 'Oxnard',
-    address3: 'CA',
-    address4: '93036',
-    itemCount: '0',
-    phoneNumber: '(805) 485-9556',
-    distance: '0.02 miles'
+    storeName: 'Lewisville',
+    itemCount: '0'
   },
   {
     itemNumber: '',
-    storeNumber: 'PPG Paints 8089',
-    address1: '921 East Ventura Blvd.',
-    address2: 'Oxnard',
-    address3: 'CA',
-    address4: '93036',
-    itemCount: '3544',
-    phoneNumber: '(805) 485-9556',
-    distance: '0.02 miles'
+    storeName: 'Aurora',
+    itemCount: '3544'
   },
   {
     itemNumber: '',
-    storeNumber: 'PPG Paints 8089',
-    address1: '921 East Ventura Blvd.',
-    address2: 'Oxnard',
-    address3: 'CA',
-    address4: '93036',
-    itemCount: '0',
-    phoneNumber: '(805) 485-9556',
-    distance: '0.02 miles'
+    storeName: 'Houston',
+    itemCount: '0'
   },
   {
     itemNumber: '',
-    storeNumber: 'PPG Paints 8089',
-    address1: '921 East Ventura Blvd.',
-    address2: 'Oxnard',
-    address3: 'CA',
-    address4: '93036',
-    itemCount: '2312',
-    phoneNumber: '(805) 485-9556',
-    distance: '0.02 miles'
+    storeName: 'Fairburn',
+    itemCount: '2312'
   },
   {
     itemNumber: '',
-    storeNumber: 'PPG Paints 8089',
-    address1: '921 East Ventura Blvd.',
-    address2: 'Oxnard',
-    address3: 'CA',
-    address4: '93036',
-    itemCount: '7496',
-    phoneNumber: '(805) 485-9556',
-    distance: '0.02 miles'
+    storeName: 'Flower Mound',
+    itemCount: '7496'
   }
 ];
 
@@ -130,9 +60,8 @@ const data: ProductInfo[] = [
 })
 export class InventoryLookupDistributionComponent implements OnInit {
   currentWindowWidth: number;
-  currentIndex: number = null;
 
-  displayedColumns = ['storeNumber', 'itemNumber'];
+  displayedColumns = ['storeName', 'itemNumber'];
   dataSource = data;
 
   constructor() {}
